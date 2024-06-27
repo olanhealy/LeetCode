8b203c2638cb835bb7ec392248549834ae115205
Olan Healy <olanhealy12@gmail.com>
2024-06-27 15:38:15 +0100

    JAVA: Running Sum of 1d Array

---
+++ Java/Easy/1480/1480.java
@@ -0,0 +1,11 @@
+class Solution {
+     public int[] runningSum(int[] nums) { 
+	  int[] solution = new int[nums.length ];
+	  solution[0] = nums[0];
+	  for (int i = 1; i < nums.length; i++) {
+		  solution[i] = solution[i - 1] + nums[i];
+	  }
+	  return solution;
+     }
+}
+
---
+++ Java/Easy/1480/notes.md
@@ -0,0 +1,19 @@
+# Notes
+
+Runtime
+0
+ms
+
+
+Beats
+100.00%
+
+
+
+Memory
+42.66
+MB
+
+
+Beats
+27.02%
